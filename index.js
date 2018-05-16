f6441d75f9ad69cacf9b44d56a1857c5357cf602
jmblargent <jmblargent@gmail.com>
2018-05-15 22:09:12 -0500

    Added move message functionality

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const Discord = require("discord.js");
+const js = require("jsearch");
+const request = require("request");
+const fs = require('fs');
 
-const TOKEN = "NDM3MDA1MzQzNDMyNjM4NDY1.DbvweA.r1qktUgwBooIml12qK5gOO7MYjI";
-const PREFIX = "Computer";
+const config = require("./config.json")
+const PREFIX = config.prefix;
 
 var bot = new Discord.Client();
 
@@ -10,12 +13,38 @@ bot.on("ready", function () {
 });
 bot.on("message", function (message) {
     if (message.author.equals(bot.user)) return;
-    if (!(message.content.startsWith(PREFIX) || message.content.startsWith(PREFIX.toLowerCase()))) return;
+    if (!(message.content.startsWith(config.prefix) || message.content.startsWith(config.prefix.toLowerCase()))) return;
     var args = message.content.substring(PREFIX.length).split(" ");
-    switch (args[0].toLowerCase()) {
+    switch (args[1].toLowerCase()) {
+        case "wrongchat":
+            console.log("moving messages");
+            var numberOfMessages = args[2];
+            var newChannel = args[3];
+            wrongChat(numberOfMessages, message, newChannel);
+            break;
         default:
+            console.log(args[1]);
             message.channel.sendMessage("Beep Boop, I don't have that functionality, yet. Have you tried suggesting it in the bot suggestion channel?");
             break;
     }
 });
-bot.login(TOKEN);
+
+bot.login(config.token);
+
+var wrongChat = function (numMessages, message, channel) {
+    console.log("Moving " + numMessages + " to " + channel);
+    numMessages = Number(numMessages) + 1;
+    console.log(numMessages);
+    message.channel.fetchMessages({ limit: numMessages })
+        .then(messages => {
+            let messagesArr = messages.array();
+            let messageCount = messagesArr.length;
+            for (let i = messageCount-1; i >= 1; i--) {
+                //console.log(messagesArr[i]);
+                bot.channels.find("name", channel).send(messagesArr[i].content);
+                messagesArr[i].delete();
+            }
+        })
+        .catch(console.error);
+}
+    
